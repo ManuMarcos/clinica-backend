c6cf2e81aaf54f770f64bd12a2ca87d5f2b423f6
ManuMarcos <manuelignaciomarcos@hotmail.com>
2025-02-11 10:41:25 -0300

    Post contruct to print enviroment variables

--- src/main/java/com/hackacode/clinica/ClinicaApplication.java
+++ src/main/java/com/hackacode/clinica/ClinicaApplication.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,4 +22,13 @@ public class ClinicaApplication {
 		SpringApplication.run(ClinicaApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logEnvVariables() {
+		System.out.println("JWT_SECRET: " + System.getenv("JWT_SECRET"));
+		System.out.println("DB URL: " + System.getenv("DB_URL"));
+		System.out.println("DB NAME: " + System.getenv("DB_NAME"));
+		System.out.println("DB USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB PASSWORD: " + System.getenv("DB_PASSWORD"));
+		System.out.println("DB PORT: " + System.getenv("DB_PORT"));
+	}
 }
