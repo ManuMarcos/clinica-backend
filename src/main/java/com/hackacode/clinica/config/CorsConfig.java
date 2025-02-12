190793ff9eaf3957a8973483c39a64b65a380d73
ManuMarcos <manuelignaciomarcos@hotmail.com>
2025-02-12 01:11:35 -0300

    Fix: Cors filter bad configuration

--- src/main/java/com/hackacode/clinica/config/CorsConfig.java
+++ src/main/java/com/hackacode/clinica/config/CorsConfig.java
@@ -1,9 +1,9 @@
 package com.hackacode.clinica.config;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -12,14 +12,15 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-        config.addAllowedOrigin(String.valueOf(List.of("http://localhost:5173")));
-        config.addAllowedHeader(String.valueOf(List.of("Origin", "Content-Type", "Accept", "Authorization")));
-        config.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter();
+        return new CorsFilter(source);
     }
-
 }
+
+
